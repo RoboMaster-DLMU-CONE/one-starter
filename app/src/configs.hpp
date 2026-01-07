dd746dc15d3e91ba466de068db4776a0b7886b7c
WindWeaver <moonbite233@gmail.com>
2026-01-07 21:22:06 +0800

    add configs file and node/hub startup code

---
+++ app/src/configs.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+// 在这里添加各个Hub的编译期Config
+// 你可以在 one-framework 文档中查找到各个Hub所需的配置
+
+
+// 在这里配置 Nodes
+void config_nodes ()
+{
+
+}
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,5 +1,9 @@
+#include <OF/lib/HubManager/HubRegistry.hpp>
+#include <OF/lib/Node/NodeManager.hpp>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+
+#include "configs.hpp"
 
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
@@ -7,6 +11,9 @@ using namespace OF;
 
 int main() {
   LOG_INF("main");
+  HubRegistry::startAll();
+  config_nodes();
+  start_all_nodes();
   while (true) {
     k_sleep(K_MSEC(1000));
   }
